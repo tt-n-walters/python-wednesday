b4e676965179c0ad03477de660a4f6eff7201ba1
ttnw <nico.walters@techtalents.es>
2022-02-16 19:31:11 +0100

    Finished off encryption/decryption

--- encryption.py
+++ encryption.py
@@ -1,57 +1,63 @@
 
-# chr()
-# Number -> Letter
+def encode():
+    global numbers
+    # Convert a whole message to numbers
+    # Encoding
+    numbers = []
+    print("Encoding..")
+    for i in range(len(message)):
+        letter = message[i]
+        n = ord(letter)
+        numbers.append(n)
 
-# ord()
-# Letter - Number
-
-# for i in range(1, 10000):
-#     print(i, chr(i))
-
-# print(ord("h"))
-
-
-
-# Convert a whole message to numbers
-# Encoding
-print("Enter message to be encoded:")
-message = "Hello world!"
-
-numbers = []
-print("Encoding..")
-for i in range(len(message)):
-    letter = message[i]
-    n = ord(letter)
-    numbers.append(n)
-
-print(numbers)
+    print(numbers)
 
 
-
-# Convert numbers to a whole message
-# Decoding
-numbers = [
-    108, 117, 106, 118, 107, 108, 107, 32, 112, 117, 32, 104, 117, 32, 104, 117, 106, 112, 108, 117,
-    97, 32, 121, 118, 116, 104, 117, 32, 106, 112, 119, 111, 108, 121,
-]
-
-for j in range(26):
-    for i in range(len(numbers)):
-        if not numbers[i] == 32:
-            numbers[i] -= 1
-            if numbers[i] < 97:
-                numbers[i] += 26
-
-
-    # print(numbers)
-
+def decode():
+    global message
+    # Convert numbers to a whole message
+    # Decoding
     letters = []
-    # print("Decoding...")
+    print("Decoding...")
     for i in range(len(numbers)):
         n = numbers[i]
         letter = chr(n)
         letters.append(letter)
 
-
     message = "".join(letters)
-    print(j, message)
+    print(message)
+
+
+def decrypt(offset):
+    print("Decrypting...")
+    for i in range(len(numbers)):
+        if not numbers[i] == 32:
+            numbers[i] -= offset
+            if numbers[i] < 97:
+                numbers[i] += 26
+
+
+def encrypt(offset):
+    print("Encrypting...")
+    for i in range(len(numbers)):
+        if not numbers[i] == 32:
+            numbers[i] += offset
+            if numbers[i] > 122:
+                numbers[i] -= 26
+
+
+print("Enter message to be encoded:")
+message = "hello world"
+
+numbers = [
+    108, 117, 106, 118, 107, 108, 107, 32, 112, 117, 32, 104, 117, 32, 104, 117, 106, 112, 108, 117,
+    97, 32, 121, 118, 116, 104, 117, 32, 106, 112, 119, 111, 108, 121,
+]
+
+encode()
+encrypt(10)
+decode()
+
+encode()
+decrypt(10)
+decode()
